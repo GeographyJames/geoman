8f74f012bbaa02f40dc3c65646571fcf2638f65d
James <james.e.campbell@outlook.com>
2025-10-23 16:19:14 +0100

    updated GitHub workflow

--- geoman/tests/app/auth/clerk.rs
+++ geoman/tests/app/auth/clerk.rs
@@ -1,5 +1,8 @@
 use secrecy::{ExposeSecret, SecretBox};
 use serde::Deserialize;
+use tokio::sync::OnceCell;
+
+static TEST_SESSION: OnceCell<ClerkSession> = OnceCell::const_new();
 
 #[derive(Deserialize)]
 struct ClerkSession {
@@ -16,23 +19,33 @@ pub struct ClerkAuthProvider {
     pub user_id: String,
 }
 
+async fn get_session(
+    client: &reqwest::Client,
+    secret: &SecretBox<String>,
+    user_id: &str,
+) -> ClerkSession {
+    let response = client
+        .post("https://api.clerk.com/v1/sessions")
+        .header("Content-Type", "application/json")
+        .header(
+            "Authorization",
+            format!("Bearer {}", secret.expose_secret()),
+        )
+        .json(&serde_json::json!({
+            "user_id": user_id
+        }))
+        .send()
+        .await
+        .expect("failed to execute request for Clerk session");
+
+    response.json().await.expect("failed to deserialise json")
+}
+
 impl ClerkAuthProvider {
     pub async fn get_test_session_token(&self, client: &reqwest::Client) -> String {
-        let response = client
-            .post("https://api.clerk.com/v1/sessions")
-            .header("Content-Type", "application/json")
-            .header(
-                "Authorization",
-                format!("Bearer {}", self.secret.expose_secret().clone()),
-            )
-            .json(&serde_json::json!({
-                "user_id": self.user_id
-            }))
-            .send()
-            .await
-            .expect("failed to execute request for Clerk session");
-
-        let session: ClerkSession = response.json().await.expect("failed to deserialise json");
+        let session = TEST_SESSION
+            .get_or_init(|| async { get_session(client, &self.secret, &self.user_id).await })
+            .await;
         let response = client
             .post(format!(
                 "https://api.clerk.com/v1/sessions/{}/tokens",
@@ -41,7 +54,7 @@ impl ClerkAuthProvider {
             .header("Content-Type", "application/json")
             .header(
                 "Authorization",
-                format!("Bearer {}", self.secret.expose_secret().clone()),
+                format!("Bearer {}", self.secret.expose_secret()),
             )
             .json(&serde_json::json!({
                 "expires_in_seconds": null
