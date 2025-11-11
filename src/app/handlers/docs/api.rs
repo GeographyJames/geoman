8d231254a86ffe63baea84ff899e67b3887ff029
James <james.e.campbell@outlook.com>
2025-11-11 05:43:22 +0000

    clippy

--- src/app/handlers/docs/api.rs
+++ src/app/handlers/docs/api.rs
@@ -5,7 +5,7 @@ use utoipa::{OpenApi, openapi::OpenApi as ApiDocs};
 
 use crate::app::ApiDoc;
 
-static API_DOC: LazyLock<ApiDocs> = LazyLock::new(|| ApiDoc::openapi());
+static API_DOC: LazyLock<ApiDocs> = LazyLock::new(ApiDoc::openapi);
 
 #[tracing::instrument]
 pub async fn get_api_docs() -> web::Json<&'static ApiDocs> {
