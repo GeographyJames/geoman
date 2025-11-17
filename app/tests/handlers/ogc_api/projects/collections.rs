3dde5802047065895d7d3f74f59cba33ae993a37
James <james.e.campbell@outlook.com>
2025-11-17 18:38:55 +0000

    fixed test

--- app/tests/handlers/ogc_api/projects/collections.rs
+++ app/tests/handlers/ogc_api/projects/collections.rs
@@ -46,7 +46,10 @@ async fn get_collections_only_returns_collectinos_that_contain_items_for_the_pro
 async fn get_project_collection_works() {
     let app = TestApp::spawn_with_db().await;
     let (_, user_id, project_id) = app.generate_ids().await;
-    let (slug, _) = app.generate_collection_slug_and_id(user_id).await;
+    let (slug, collection_id) = app.generate_collection_slug_and_id(user_id).await;
+    let _feature_id = app
+        .generate_feature_id(collection_id, project_id, user_id, Some({}))
+        .await;
     let response = app
         .ogc_service
         .get_project_collection(&app.api_client, &project_id.into(), &slug)
